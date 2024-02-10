b0cdbefbc69a1ccd84da6d0e6a8fa9d928f693e9
UjwalVarma <120555973+UjwalVarma@users.noreply.github.com>
2024-02-10 20:37:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fibonacci(num) {
     if (num === 0) {
         return 0;
     } else if (num === 1) {
-        return 1;
+        return 0;
     } else {
         let a = 0, b = 1;
         for (let i = 2; i < num; i++) {
