91feb8a5b1cf16939434701771a449984536b9b5
UjwalVarma <120555973+UjwalVarma@users.noreply.github.com>
2024-02-10 20:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 function fibonacci(num) {
-// your code here
-  if (num === 0) {
-    return 0;
-  } else if (num === 1) {
-    return 1;
-  } else {
-    let a = 0;
-    let b = 1;
-
-    for (let i = 2; i <= num; i++) {
-      let temp = a + b;
-      a = b;
-      b = temp;
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        let a = 0, b = 1;
+        for (let i = 2; i < num; i++) {
+            let temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return b;
     }
-
-    return b;
-  }
 }
+
+// Examples
 console.log(fibonacci(1)); 
 console.log(fibonacci(5)); 
-
 
 module.exports = fibonacci;
